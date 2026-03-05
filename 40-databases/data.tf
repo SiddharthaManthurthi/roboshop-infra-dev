82ac08db34c663525e531bcd48a0e668b1256240
SiddharthaManthurthi <manthurthisiddhartha@gmail.com>
2026-03-05 14:07:26 +0530

    terraform

--- 40-databases/data.tf
+++ 40-databases/data.tf
@@ -19,9 +19,9 @@ data "aws_ami" "joindevops" {
 }
 
 data "aws_ssm_parameter" "database_subnet_ids" {
-    name = "/${var.project}/${var.environment}/public_subnet_ids"
+    name = "/${var.project}/${var.environment}/database_subnet_ids"
 }
 
 data "aws_ssm_parameter" "mongodb_sg_id" {
-    name = "/${var.project}/${var.environment}/bastion_sg_id"
+    name = "/${var.project}/${var.environment}/mongodb_sg_id"
 }

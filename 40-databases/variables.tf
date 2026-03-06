af1f4dadb73712949955dcb66942d088202fe3b2
SiddharthaManthurthi <manthurthisiddhartha@gmail.com>
2026-03-06 13:44:08 +0530

    terraform

--- 40-databases/variables.tf
+++ 40-databases/variables.tf
@@ -5,3 +5,11 @@ variable "project" {
 variable "environment" {
     default = "dev"
 }
+
+variable "zone_id" {
+    default = "Z0397543RCC8GJCVQ50L"
+}
+
+variable "domain_name" {
+    default = "siddharthais.online"
+}

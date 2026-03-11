2484a31c4b1cb2712f9bc7843e978ea6058d062f
SiddharthaManthurthi <manthurthisiddhartha@gmail.com>
2026-03-11 11:55:55 +0530

    terraform

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -32,7 +32,7 @@ resource "terraform_data" "catalogue" {
   provisioner "remote-exec" {
     inline = [
         "chmod +x /tmp/bootstrap.sh",
-        "sudo sh /tmp/bootstrap.sh mongodb"
+        "sudo sh /tmp/bootstrap.sh catalogue"
     ]
   }
 }

af53d5d743625462b88ea1f79629b0f71dbf8e18
SiddharthaManthurthi <manthurthisiddhartha@gmail.com>
2026-03-20 13:21:02 +0530

    terraform

--- 80-frontend-alb/main.tf
+++ 80-frontend-alb/main.tf
@@ -28,7 +28,9 @@ resource "aws_lb_listener" "https" {
 
     fixed_response {
       content_type = "text/html"
-      message_body = "<div style=\"text-align:center;font-family:Arial;\"><h1 style=\"color:red !important;font-size:42px !important;\">I Love You, Ghreeshma &#10084;&#10084;&#10084;</h1><p style=\"color:black !important;font-size:24px !important;font-weight:bold;\">Miss you da Ghreeshma :(</p><p style=\"font-size:18px !important;\">Yours lovingly, Siddhu &#128151;</p></div>"
+      message_body = "<h1>Hi, I am from HTTPS Frontend ALB</h1>"
+      
+      #"<div style=\"text-align:center;font-family:Arial;\"><h1 style=\"color:red !important;font-size:42px !important;\">I Love You, Ghreeshma &#10084;&#10084;&#10084;</h1><p style=\"color:black !important;font-size:24px !important;font-weight:bold;\">Miss you da Ghreeshma :(</p><p style=\"font-size:18px !important;\">Yours lovingly, Siddhu &#128151;</p></div>"
       status_code  = "200"
     }
   }

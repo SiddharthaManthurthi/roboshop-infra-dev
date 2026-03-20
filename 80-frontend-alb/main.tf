39794876ea09ba94614c4195b39ec83a20701b00
SiddharthaManthurthi <manthurthisiddhartha@gmail.com>
2026-03-20 10:55:34 +0530

    terraform

--- 80-frontend-alb/main.tf
+++ 80-frontend-alb/main.tf
@@ -1,9 +1,9 @@
-resource "aws_lb" "backend_alb" {
-  name               = "${var.project}-${var.environment}" # roboshop-dev
+resource "aws_lb" "frontend_alb" {
+  name               = "${var.project}-${var.environment}-frontend" # roboshop-dev-frontend
   internal           = true
   load_balancer_type = "application"
-  security_groups    = [local.backend_alb_sg_id]
-  subnets            = local.private_subnet_ids
+  security_groups    = [local.frontend_alb_sg_id]
+  subnets            = local.public_subnet_ids
 
   # keeping it as false, just to delete using terraform while practice
   enable_deletion_protection = false
@@ -17,7 +17,7 @@ resource "aws_lb" "backend_alb" {
 }
 
 resource "aws_lb_listener" "http" {
-  load_balancer_arn = aws_lb.backend_alb.arn
+  load_balancer_arn = aws_lb.frontend_alb.arn
   port              = "80"
   protocol          = "HTTP"
 
@@ -39,8 +39,8 @@ resource "aws_route53_record" "www" {
   
   # load balancer details
   alias {
-    name                   = aws_lb.backend_alb.dns_name
-    zone_id                = aws_lb.backend_alb.zone_id
+    name                   = aws_lb.frontend_alb.dns_name
+    zone_id                = aws_lb.frontend_alb.zone_id
     evaluate_target_health = true
   }
 }

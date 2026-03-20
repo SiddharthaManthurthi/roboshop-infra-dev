4a64b4128bacb5dfed3e658ea273602e5c5641c7
SiddharthaManthurthi <manthurthisiddhartha@gmail.com>
2026-03-20 10:52:28 +0530

    terraform

---
+++ 80-frontend-alb/variables.tf
@@ -0,0 +1,15 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z0397543RCC8GJCVQ50L"
+}
+
+variable "domain_name" {
+    default = "siddharthais.online"
+}

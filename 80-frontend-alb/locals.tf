087b3de499c60cb1d27dbbd89dd6ffa2c6a79611
SiddharthaManthurthi <manthurthisiddhartha@gmail.com>
2026-03-20 10:53:36 +0530

    terraform

--- 80-frontend-alb/locals.tf
+++ 80-frontend-alb/locals.tf
@@ -1,6 +1,6 @@
 locals {
-  backend_alb_sg_id = data.aws_ssm_parameter.backend_alb_sg_id.value
-  private_subnet_ids = split(",", data.aws_ssm_parameter.private_subnet_ids.value)
+  frontend_alb_sg_id = data.aws_ssm_parameter.frontend_alb_sg_id.value
+  public_subnet_ids = split(",", data.aws_ssm_parameter.public_subnet_ids.value)
   common_tags = {
         Project = var.project
         Environment = var.environment

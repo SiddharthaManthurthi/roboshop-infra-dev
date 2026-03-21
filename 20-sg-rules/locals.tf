2586672fb62253a4578ac311c48033257d75b30e
SiddharthaManthurthi <manthurthisiddhartha@gmail.com>
2026-03-21 12:15:26 +0530

    terraform

--- 20-sg-rules/locals.tf
+++ 20-sg-rules/locals.tf
@@ -9,6 +9,7 @@ locals {
   user_sg_id = data.aws_ssm_parameter.user_sg_id.value
   cart_sg_id = data.aws_ssm_parameter.cart_sg_id.value
   shipping_sg_id = data.aws_ssm_parameter.shipping_sg_id.value
+  payment_sg_id = data.aws_ssm_parameter.payment_sg_id.value
   backend_alb_sg_id = data.aws_ssm_parameter.backend_alb_sg_id.value
   frontend_alb_sg_id = data.aws_ssm_parameter.frontend_alb_sg_id.value
 }

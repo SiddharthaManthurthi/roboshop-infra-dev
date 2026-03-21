42c253c7f8b902d6bd07f6dd8ad9d01ba8fd117c
SiddharthaManthurthi <manthurthisiddhartha@gmail.com>
2026-03-21 12:19:12 +0530

    terraform

--- 20-sg-rules/main.tf
+++ 20-sg-rules/main.tf
@@ -280,6 +280,16 @@ resource "aws_security_group_rule" "backend_alb_frontend" {
   security_group_id = local.backend_alb_sg_id
 }
 
+resource "aws_security_group_rule" "frontend_frontend_alb" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  # Where traffic is coming from
+  source_security_group_id = local.frontend_alb_sg_id
+  security_group_id = local.frontend_sg_id
+}
+
 resource "aws_security_group_rule" "frontend_alb_public" {
   type              = "ingress"
   from_port         = 443

a60aa6e525e29872dfc683738f3a9d28bbd02f15
SiddharthaManthurthi <manthurthisiddhartha@gmail.com>
2026-03-21 11:55:26 +0530

    terraform

--- 20-sg-rules/main.tf
+++ 20-sg-rules/main.tf
@@ -108,15 +108,7 @@ resource "aws_security_group_rule" "rabbitmq_payment" {
   security_group_id = local.rabbitmq_sg_id
 }
 
-resource "aws_security_group_rule" "backend_alb_bastion" {
-  type              = "ingress"
-  from_port         = 80
-  to_port           = 80
-  protocol          = "tcp"
-  # Where traffic is coming from
-  source_security_group_id = local.bastion_sg_id
-  security_group_id = local.backend_alb_sg_id
-}
+
 
 resource "aws_security_group_rule" "catalogue_bastion" {
   type              = "ingress"
@@ -136,6 +128,16 @@ resource "aws_security_group_rule" "catalogue_backend_alb" {
   # Where traffic is coming from
   source_security_group_id = local.backend_alb_sg_id
   security_group_id = local.catalogue_sg_id
+}
+
+resource "aws_security_group_rule" "backend_alb_bastion" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  # Where traffic is coming from
+  source_security_group_id = local.bastion_sg_id
+  security_group_id = local.backend_alb_sg_id
 }
 
 resource "aws_security_group_rule" "frontend_alb_public" {

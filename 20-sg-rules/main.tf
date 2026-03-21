11f242cb6f876e9e28ab3d37a89203b290db3126
SiddharthaManthurthi <manthurthisiddhartha@gmail.com>
2026-03-21 12:03:10 +0530

    terraform

--- 20-sg-rules/main.tf
+++ 20-sg-rules/main.tf
@@ -170,6 +170,28 @@ resource "aws_security_group_rule" "cart_backend_alb" {
   security_group_id = local.cart_sg_id
 }
 
+resource "aws_security_group_rule" "shipping_bastion" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  # Where traffic is coming from
+  source_security_group_id = local.bastion_sg_id
+  security_group_id = local.shipping_sg_id
+}
+
+resource "aws_security_group_rule" "shipping_backend_alb" {
+  type              = "ingress"
+  from_port         = 8080
+  to_port           = 8080
+  protocol          = "tcp"
+  # Where traffic is coming from
+  source_security_group_id = local.backend_alb_sg_id
+  security_group_id = local.shipping_sg_id
+}
+
+
+
 resource "aws_security_group_rule" "backend_alb_bastion" {
   type              = "ingress"
   from_port         = 80

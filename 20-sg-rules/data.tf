ba88724ea27a02cad67011efb45a3f290724efba
SiddharthaManthurthi <manthurthisiddhartha@gmail.com>
2026-03-21 12:10:32 +0530

    terraform

--- 20-sg-rules/data.tf
+++ 20-sg-rules/data.tf
@@ -34,6 +34,10 @@ data "aws_ssm_parameter" "user_sg_id" {
     name = "/${var.project}/${var.environment}/user_sg_id"
 }
 
+data "aws_ssm_parameter" "cart_sg_id" {
+    name = "/${var.project}/${var.environment}/cart_sg_id"
+}
+
 data "aws_ssm_parameter" "backend_alb_sg_id" {
     name = "/${var.project}/${var.environment}/backend_alb_sg_id"
 }

70b35d4c457dfefacb0774a5991d0c8545dbb597
SiddharthaManthurthi <manthurthisiddhartha@gmail.com>
2026-03-24 15:38:47 +0530

    terraform

--- 98-openvpn/locals.tf
+++ 98-openvpn/locals.tf
@@ -1,11 +1,24 @@
+# locals {
+#     ami_id =  data.aws_ami.openvpn.id
+#     common_tags = {
+#         Project = var.project
+#         Environment = var.environment
+#         Terraform = "true"
+#     }
+#     # public subnet in 1a AZ
+#     public_subnet_id = split(",", data.aws_ssm_parameter.public_subnet_ids.value)[0]
+#     openvpn_sg_id = data.aws_ssm_parameter.open_vpn_sg_id.value
+# }
+
 locals {
-    ami_id =  data.aws_ami.openvpn.id
+    ami_id = data.aws_ami.openvpn.id
+    openvpn_sg_id = data.aws_ssm_parameter.openvpn_sg_id.value
+    public_subnet_id = split("," , data.aws_ssm_parameter.public_subnet_ids.value)[0]
     common_tags = {
         Project = var.project
         Environment = var.environment
         Terraform = "true"
     }
-    # public subnet in 1a AZ
-    public_subnet_id = split(",", data.aws_ssm_parameter.public_subnet_ids.value)[0]
-    openvpn_sg_id = data.aws_ssm_parameter.open_vpn_sg_id.value
+
+    
 }

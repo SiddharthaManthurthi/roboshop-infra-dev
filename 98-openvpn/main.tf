7bcf247fea9ee53a614ac7eb7c488151c8a087ba
SiddharthaManthurthi <manthurthisiddhartha@gmail.com>
2026-03-24 15:32:42 +0530

    terraform

--- 98-openvpn/main.tf
+++ 98-openvpn/main.tf
@@ -1,14 +1,30 @@
-resource "aws_instance" "openvpn" {
-  ami           = local.ami_id
-  instance_type = "t3.small"
-  subnet_id = local.public_subnet_id
-  vpc_security_group_ids = [local.openvpn_sg_id]
-  user_data = file("vpn.sh")
+# resource "aws_instance" "openvpn" {
+#   ami           = local.ami_id
+#   instance_type = "t3.small"
+#   subnet_id = local.public_subnet_id
+#   vpc_security_group_ids = [local.openvpn_sg_id]
+#   user_data = file("vpn.sh")
 
-  tags = merge(
-    {
-        Name = "${var.project}-${var.environment}-openvpn"
-    },
-    local.common_tags
-  )
+#   tags = merge(
+#     {
+#         Name = "${var.project}-${var.environment}-openvpn"
+#     },
+#     local.common_tags
+#   )
+# }
+
+
+resource "aws_instance" "openvpn" {
+    ami = local.ami_id
+    instance_type = "t3.micro"
+    vpc_security_group_ids = [local.openvpn_sg_id]
+    subnet_id = local.public_subnet_id
+    user_data = file("vpn.sh")
+    
+    tags = merge (
+        local.common_tags,
+        {
+            Name = "${var.project}-${var.environment}-openvpn"
+        }
+    )
 }
